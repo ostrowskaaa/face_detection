8a5862126bbdb1a3431c078a1b2447a8f712a377
Aga <ag.ostrowskaaa@gmail.com>
2019-11-17 22:41:13 +0100

    add gallery and description, remove upload

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-from .forms import UploadImageForm
 from .forms import ImageUploadForm
 from .dface import dface
 
@@ -11,17 +10,8 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def upload(request):
-    form = UploadImageForm(request.POST, request.FILES)
-    if form.is_valid():
-        myfile = request.FILES['image']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'upload.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
-    else:
-        form = UploadImageForm()
-        return render(request, 'upload.html', {'form': form})
+def gallery(request):
+    return render(request, 'gallery.html')
 
 def detection(request):
     form = ImageUploadForm(request.POST, request.FILES)
