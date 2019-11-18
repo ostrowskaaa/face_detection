8b5821e2b9da0372114a65d9b562656a6853c975
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-11-18 23:20:15 +0100

    Update forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,12 +1,7 @@
 from django import forms
 from .models import ImageUploadModel
 
-class UploadImageForm(forms.Form):
-    title=forms.CharField(max_length=50)
-    #file = forms.FileField()
-    image=forms.ImageField()
-
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUploadModel
-        fields = ('description', 'document', )
+        fields = ('title', 'description', 'document', )
