e4f68268b6de4d9c35a33bd452bfe4e106cbc0b6
ziebisty <50321799+ziebisty@users.noreply.github.com>
2019-11-18 09:28:53 +0100

    completed page sections

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,9 +6,10 @@ from . import views
 
 urlpatterns = [
     url('^$', views.home, name='home'),
+    url('gallery/', views.gallery, name='gallery'),
     url('detection/', views.detection, name='detection'),
-    url('upload/', views.upload, name='upload'),
     url('about/', views.about, name='about'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- webapp/views.py
+++ webapp/views.py
@@ -11,29 +11,21 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def upload(request):
-    form = UploadImageForm(request.POST, request.FILES)
-    if form.is_valid():
-        myfile = request.FILES['image']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'upload.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
-    else:
-        form = UploadImageForm()
-        return render(request, 'upload.html', {'form': form})
-
 def detection(request):
     form = ImageUploadForm(request.POST, request.FILES)
     if request.method == 'POST':
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-    
+
             imageURL = settings.MEDIA_URL + form.instance.document.name
             dface(settings.MEDIA_ROOT_URL + imageURL)
-    
+
             return render(request, 'detection.html', {'form':form, 'post':post})
     else:
         form = ImageUploadForm()
     return render(request, 'detection.html',{'form':form})
+
+def gallery(request):
+
+    return render(request, 'gallery.html')
