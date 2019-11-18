1395d2a0f58703f6ace7a664d6a4dab7aaef5fa0
ziebisty <50321799+ziebisty@users.noreply.github.com>
2019-11-18 08:23:18 +0100

    merging unwanted branches

--- webapp/models.py
+++ webapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class ImageUploadModel(models.Model):
-    title = models.CharField(max_length=50, blank=True)
     description = models.CharField(max_length=255, blank=True)
     document = models.ImageField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
