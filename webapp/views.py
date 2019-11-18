6ef55f3ab91c9b11cb973e3452b7a132716a705b
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-11-18 08:54:00 +0100

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-from .forms import UploadImageForm
 from .forms import ImageUploadForm
 from .dface import dface
+from .dface import gallery
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
-
-def upload(request):
-    form = UploadImageForm(request.POST, request.FILES)
-    if form.is_valid():
-        myfile = request.FILES['image']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'upload.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
-    else:
-        form = UploadImageForm()
-        return render(request, 'upload.html', {'form': form})
 
 def detection(request):
     form = ImageUploadForm(request.POST, request.FILES)
@@ -37,3 +25,8 @@ def detection(request):
     else:
         form = ImageUploadForm()
     return render(request, 'detection.html',{'form':form})
+
+def gallery(request):
+    imageURL = settings.MEDIA_URL + form.instance.document.name
+    gallery(settings.MEDIA_ROOT_URL + imageURL)
+    return render(request, 'gallery.html')
