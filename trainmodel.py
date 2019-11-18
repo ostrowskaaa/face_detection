a9e3c7496eddb19d6b70362cdf40d3c740a6e7fd
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-11-18 09:25:35 +0100

    Create trainmodel.py

---
+++ trainmodel.py
@@ -0,0 +1,26 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import argparse
+import pickle
+
+# load the face embeddings
+data = pickle.loads(open(args["embeddings"], "rb").read())
+
+# encode the labels
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# train the model used to accept the 128-d embeddings of the face and
+# then produce the actual face recognition
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+# write the actual face recognition model to disk
+f = open(args["recognizer"], "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+
+# write the label encoder to disk
+f = open(args["le"], "wb")
+f.write(pickle.dumps(le))
+f.close()
