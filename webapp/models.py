bb50e1887cb233dffe34c43895740763a5d6c8df
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-11-18 23:19:44 +0100

    Update models.py

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class ImageUploadModel(models.Model):
+    title = models.CharField(max_length=50, blank=True)
     description = models.CharField(max_length=255, blank=True)
     document = models.ImageField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
