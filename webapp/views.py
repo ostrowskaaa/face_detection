d332cbe463f7ee24717f3ba58cf36ba073a9acb8
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2019-11-19 20:26:18 +0100

    nearly gallery

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,12 @@
+import os
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from .forms import ImageUploadForm
+#from .forms import GalleryForm
 from .dface import dface
-from .dface import gallery
+#from .dface import gallery
+from cv2 import cv2
 
 def home(request):
     return render(request, 'home.html')
@@ -17,16 +20,20 @@ def detection(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
-    
+
             imageURL = settings.MEDIA_URL + form.instance.document.name
             dface(settings.MEDIA_ROOT_URL + imageURL)
-    
+
             return render(request, 'detection.html', {'form':form, 'post':post})
     else:
         form = ImageUploadForm()
     return render(request, 'detection.html',{'form':form})
 
 def gallery(request):
-    imageURL = settings.MEDIA_URL + form.instance.document.name
-    gallery(settings.MEDIA_ROOT_URL + imageURL)
-    return render(request, 'gallery.html')
+    imageURL = settings.MEDIA_ROOT_URL + settings.MEDIA_URL + 'documents/'
+    img_list = os.listdir(imageURL)
+    for file in img_list:
+
+        img = cv2.imread(imageURL+file, 1)
+       # cv2.imwrite(imageURL+file, img)
+    return render(request, "gallery.html", {'documents': img_list})
